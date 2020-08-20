e99ca2d40ab68066220872e31632171c3d806410
Nathan <nathanluc00@gmail.com>
2020-08-19 23:21:37 -0700

    Added Console Data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import WeatherCard from './Components/WeatherCard/component';
 function App() {
   const data = async() => {
     const apiRes = await fetch(
-      "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=354c35ca53bb100ba6f404e49d0bba03"
+      "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=354c35ca53bb100ba6f404e49d0bba03"
     );
     const resJSON = await apiRes.json();
     return resJSON;
   }
+    console.log(data());
   return (
     <div className="App">
      <WeatherCard city = 'San Jose' country = 'United States' temp = {30} condition = "Tornado" />
